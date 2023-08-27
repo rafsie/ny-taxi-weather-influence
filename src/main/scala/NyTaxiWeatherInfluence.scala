77a776294f364a81dbeceff1aa9a8858c6c44ed8
rafsie <57662562+rafsie@users.noreply.github.com>
2023-08-27 17:20:49 +0200

    Functions and logic

--- src/main/scala/NyTaxiWeatherInfluence.scala
+++ src/main/scala/NyTaxiWeatherInfluence.scala
@@ -5,7 +5,7 @@ object NyTaxiWeatherInfluence extends App {
 
   val spark = SparkSession.builder()
     .config("spark.master", "local[*]")
-    .appName("NyTaxi")
+    .appName("NyTaxiWeatherInfluence")
     .getOrCreate()
 
   spark.sparkContext.setLogLevel("ERROR")
@@ -19,14 +19,31 @@ object NyTaxiWeatherInfluence extends App {
 //    .load("/home/rs/Desktop/ntfs/nytaxi/part-*.snappy.parquet")
   .load("/home/rs/Desktop/ntfs/nytaxi/part-r-00000-ec9cbb65-519d-4bdb-a918-72e2364c144c.snappy.parquet")
 
-  val nyZonesDF = spark.read
-    .option("header", "true")
-    .option("inferSchema", "true")
-    .csv("/home/rs/Desktop/ntfs/nytaxi/taxi_zone_lookup.csv")
-
   val nyWeatherDF = spark.read
     .option("header", "true")
     .option("inferSchema", "true")
     .csv("/home/rs/Desktop/ntfs/nytaxi/nyc_weather.csv")
+
+  def fahrenheitToCelsius(f: ColumnName) = round((f - 32) * 5 / 9, 1)
+  def milesToKmPerHour(m: ColumnName) = round(m * 1.609344, 1)
+  def inchesToMm(i: ColumnName) = round(i * 25.4, 1)
+
+  val weatherDF = nyTaxiDF
+    .select("pickup_datetime")
+    .withColumn("pickup_date", to_date(col("pickup_datetime"), "yyyy-MM-dd"))
+    .withColumn("DOW", date_format(col("pickup_date"), "E"))
+    .withColumn("WeekDayNo", dayofweek(col("pickup_date")))
+    .join(nyWeatherDF, col("pickup_date") === col("Date").cast("date"))
+    .groupBy($"pickup_date".as("Date"),
+      $"WeekDayNo".as("WeekDayNo"),
+      $"DOW".as("DOW"),
+      fahrenheitToCelsius($"Temp_avg").as("AvgTempC"),
+      milesToKmPerHour($"Wind_avg").as("AvgWindKmh"),
+      inchesToMm($"Precipitation").as("PrecipitationMMm2"))
+    .agg(count("*").as("TripsNo"))
+    //    .where($"PrecipitationMMm2" > 0.0)
+    .orderBy(col("Date").asc_nulls_last)
+
+  weatherDF.show(10)
 
 }
